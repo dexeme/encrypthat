d3bf2add0a0fae4ac54ed4fdc3a2a04fbd71cbed
Dexeme <dexe.me12@gmail.com>
2023-02-20 18:03:06 -0300

    Resolve AppBar and BottomNavBar creation every time you switch views

--- encrypthat/lib/main.dart
+++ encrypthat/lib/main.dart
@@ -1,8 +1,5 @@
-// ignore_for_file: prefer_const_constructors
-
-import 'package:encrypthat/routes.dart';
+import 'package:encrypthat/widgets/main_view.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,53 +10,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Desafio Labsec',
-      routerDelegate: routes.routerDelegate,
-      routeInformationParser: routes.routeInformationParser,
-      routeInformationProvider: routes.routeInformationProvider,
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+    return MaterialApp(
+      title: 'Encrypthat',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              TextButton(
-                onPressed: () => context.go('/views/ble_devices_view'),
-                child: const Text('Dispositivos BLE'),
-              ),
-              TextButton(
-                  onPressed: () => context.go('/views/keys_view'),
-                  child: const Text('Chaves RSA')),
-              TextButton(
-                  onPressed: () => context.go('/views/signature_generator_view'),
-                  child: const Text('Assinar Lista')),
-              TextButton(
-                  onPressed: () => context.go('/views/signature_validator_view'),
-                  child: const Text('Validar Assinatura')),
-            ]),
-          ],
-        ),
-      ),
+      home: const MainView(),
     );
   }
 }
--- encrypthat/lib/routes.dart
+++ encrypthat/lib/routes.dart
@@ -1,34 +1,24 @@
-import 'package:encrypthat/test.dart';
-import 'package:encrypthat/views/ble_devices_view.dart';
-import 'package:encrypthat/views/home_page_view.dart';
+import 'package:encrypthat/widgets/main_view.dart';
 import 'package:encrypthat/views/keys_page_view.dart';
 import 'package:go_router/go_router.dart';
-import 'views/signature_view.dart';
+import 'views/signature_page_view.dart';
 import 'views/signature_verifier_view.dart';
 
 final routes = GoRouter(routes: [
   GoRoute(
     path: '/',
-    builder: (context, state) => const HomePage(),
+    builder: (context, state) => const MainView(),
   ),
   GoRoute(
     path: '/views/keys_page_view',
     builder: (context, state) => const KeysPage(),
   ),
   GoRoute(
-    path: '/views/ble_devices_view',
-    builder: (context, state) => const BLEDevicesView(),
-  ),
-  GoRoute(
-    path: '/views/signature_generator_view',
-    builder: (context, state) => const SignatureGeneratorView(),
+    path: '/views/signature_page_view',
+    builder: (context, state) => const SignaturePage(),
   ),
   GoRoute(
     path: '/views/signature_validator_view',
     builder: (context, state) => const SignatureValidatorView(),
   ),
-  GoRoute(
-    path: '/test',
-    builder: (context, state) => const TestWidget(),
-  )
 ]);
