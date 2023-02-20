c5cb1a896f9adf13301db90ac2a68961da919221
Dexeme <dexe.me12@gmail.com>
2023-02-20 12:46:16 -0300

    Change signature return to Uint8List format

--- encrypthat/lib/services/encryption/signature_generator.dart
+++ encrypthat/lib/services/encryption/signature_generator.dart
@@ -1,9 +1,12 @@
-import 'dart:convert';
+// ignore_for_file: avoid_print
+
 import 'dart:typed_data';
 import 'package:pointycastle/api.dart';
 import 'package:pointycastle/asymmetric/api.dart';
 import 'package:pointycastle/digests/sha256.dart';
 import 'package:pointycastle/signers/rsa_signer.dart';
+import 'package:encrypthat/storage_manager.dart';
+import 'key_pair_generator.dart';
 
 class SignatureGenerator {
   SignatureGenerator._privateConstructor();
@@ -11,18 +14,37 @@ class SignatureGenerator {
       SignatureGenerator._privateConstructor();
   static SignatureGenerator get instance => _instance;
 
-  String? _signature;
+  final keyPairGenerator = KeyPairGenerator.instance;
+  final storage = StorageManager.instance;
 
-  String? get signature => _signature;
+// private key, data to sign, public key, signature
+  Uint8List? dataToSign;
+  Uint8List? signature;
 
-  String sign(String plainText, RSAPrivateKey privateKey) {
-    var signer = RSASigner(SHA256Digest(), "0609608648016503040201");
-    signer.init(true, PrivateKeyParameter<RSAPrivateKey>(privateKey));
+  AsymmetricKeyPair<RSAPublicKey, RSAPrivateKey>? keyPair;
 
-    _signature = base64Encode(signer
-        .generateSignature(Uint8List.fromList(plainText.codeUnits))
-        .bytes);
+  void initVariables() {}
 
-    return _signature!;
+  get getDataToSign => dataToSign;
+  get getSignature => signature;
+  get getKeyPair => keyPair;
+
+  Uint8List rsaSign(Uint8List dataToSign) {
+    final signer = RSASigner(SHA256Digest(), '0609608648016503040201');
+
+    final privateKey = keyPairGenerator.privateKey;
+    if (privateKey == null) {
+      print('Private key is null');
+      throw Exception('Private key is null');
+    }
+
+    signer.init(
+        true, PrivateKeyParameter<RSAPrivateKey>(privateKey)); // true=sign
+
+    final sig = signer.generateSignature(dataToSign);
+
+    signature = sig.bytes;
+
+    return sig.bytes;
   }
 }
