b24ae2f772a16ad492c61d2bff855046d0b3e2c5
Dexeme <dexe.me12@gmail.com>
2023-02-20 12:44:11 -0300

    Remove last scan and title info

--- encrypthat/lib/services/bluetooth/widgets/scan_result_panel.dart
+++ encrypthat/lib/services/bluetooth/widgets/scan_result_panel.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:encrypthat/constants/constants.dart' as constants;
 
 class ScanResultPanel extends StatelessWidget {
   final List<String> devicesList;
@@ -12,29 +13,32 @@ class ScanResultPanel extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const Text(
-          'Dispositivos encontrados:',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
+    return Container(
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(5)),
+        color: constants.devicesPanelColor,
+        boxShadow: [constants.panelShadow],
+      ),
+      padding: const EdgeInsets.all(0),
+      margin: const EdgeInsets.only(top: 20),
+      child: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: devicesList.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(
+                    devicesList[index].toUpperCase(),
+                    style: constants.regularFont,
+                  ),
+                  leading: const Icon(Icons.arrow_right),
+                );
+              },
+            ),
           ),
-        ),
-        Text(
-          'Última vez que o botão foi pressionado: ${lastScanTime?.toString() ?? "-"}',
-          style: const TextStyle(
-            fontStyle: FontStyle.italic,
-          ),
-        ),
-        Expanded(
-          child: ListView.builder(
-            itemCount: devicesList.length,
-            itemBuilder: (context, index) {
-              return Text(devicesList[index]);
-            },
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
