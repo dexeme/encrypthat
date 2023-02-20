ce6bded73b565a4e7d588918af9ab1a73cdb3e72
Dexeme <dexe.me12@gmail.com>
2023-02-20 18:04:49 -0300

    Add button for signing device list

---
+++ encrypthat/lib/services/encryption/widgets/sign_list_button.dart
@@ -0,0 +1,75 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'dart:async';
+import 'package:encrypthat/constants/constants.dart' as constants;
+import 'package:encrypthat/services/encryption/key_pair_generator.dart';
+import 'package:flutter/cupertino.dart';
+
+import 'package:flutter/material.dart';
+
+class SignListButton extends StatefulWidget {
+  final VoidCallback onPressed;
+
+  const SignListButton({Key? key, required this.onPressed}) : super(key: key);
+
+  @override
+  _SignListButtonState createState() => _SignListButtonState();
+}
+
+class _SignListButtonState extends State<SignListButton> {
+  bool _isButtonEnabled = true;
+  KeyPairGenerator keys = KeyPairGenerator.instance;
+  bool? _isButtonAvailable;
+
+  @override
+  void initState() {
+    super.initState();
+    _isButtonAvailableCheck();
+  }
+
+  _isButtonAvailableCheck() {
+    if (keys.publicKey == null || keys.privateKey == null) {
+      setState(() {
+        _isButtonAvailable = false;
+      });
+    } else {
+      setState(() {
+        _isButtonAvailable = true;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _isButtonAvailable!
+          ? () {
+              widget.onPressed();
+              setState(() {
+                _isButtonEnabled = false;
+              });
+              Future.delayed(const Duration(seconds: 2), () {
+                setState(() {
+                  _isButtonEnabled = true;
+                });
+              });
+            }
+          : null,
+      child: Container(
+        width: 300,
+        height: 50,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: _isButtonAvailable!
+              ? constants.buttonColor
+              : constants.buttonColorDisabled,
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: _isButtonAvailable!
+            ? const Text('ASSINAR LISTA', style: constants.startScanButtonStyle)
+            : const Text('DADOS FALTANDO!',
+                style: constants.startScanButtonStyle),
+      ),
+    );
+  }
+}
